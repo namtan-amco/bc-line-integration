0ce2567afea117b207e69660a41e48c06df0a974
Namtan <waranya@alphametrics.co.th>
2026-05-20 13:15:00 +0700

    Fix URL Submit Approval

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
 const myLiffId = "2010128261-kYvBcg1E";
 
 const urlFetchSalesLines = "https://default62c465b8d4a74552b7e2ab054bb358.6d.environment.api.powerplatform.com:443/powerautomate/automations/direct/workflows/7765aba9cc08433fad8382175f70e03e/triggers/manual/paths/invoke?api-version=1&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=wAQfzYs9QURYawYqJGCXw1C3nRmP1RRxk6OrWQ2dTOY";
-const urlSubmitApproval = "https://default62c465b8d4a74552b7e2ab054bb358.6d.environment.api.powerplatform.com:443/powerautomate/automations/direct/workflows/15c4c982bb9143aca0bf8735cd307965/triggers/manual/paths/invoke?api-version=1";
-
+const urlSubmitApproval = "https://default62c465b8d4a74552b7e2ab054bb358.6d.environment.api.powerplatform.com:443/powerautomate/automations/direct/workflows/15c4c982bb9143aca0bf8735cd307965/triggers/manual/paths/invoke?api-version=1&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=jpVZcxz_6RHycwfw_2HzC49UCYRZiIBtsfiFonKpw1g";
+                           
 // ตัวแปรเก็บค่าสำคัญที่ดักมาจากลิงก์แชท LINE
 let currentApprovalEntrySystemId = "";
 let currentDocumentNo = "";
